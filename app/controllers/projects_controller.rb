ee1deef04a71cde9fa7059d60d86b339bce29763
fernandodxx <fernandosillvasantoss@gmail.com>
2025-08-12 22:08:22 -0300

    Add edit functionality and success messages for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,11 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[show edit update]
+
   def index
     @projects = Project.all
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def new
@@ -15,13 +16,30 @@ class ProjectsController < ApplicationController
     @project = Project.new(project_params)
 
     if @project.save
+      flash[:notice] = "Project created successfully!"
       redirect_to project_path(@project)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      flash[:notice] = "Project updated successfully!"
+      redirect_to project_path(@project)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
 
   def project_params
     params.require(:project).permit(:name)
